637659ec60802be2a09b5542a9a2a5e4a3479426
Shimbo Takaaki <ta7uwtaka@gmail.com>
2017-11-05 08:48:14 +0900

    Create models

--- vote/models.py
+++ vote/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField("name", max_length=128)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Finalist(models.Model):
+    name = models.CharField("name",max_length=128)
+    Votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
