ec26ce5dce466e30a4a8c8f8e6984143997a4aab
Shimbo Takaaki <ta7uwtaka@gmail.com>
2017-12-02 23:25:25 +0900

    Add ajax_get func

--- vote/urls.py
+++ vote/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.QuestionDetail.as_view(), name='detail'),
     url(r'^(?P<pk>\d+)/vote$', views.vote, name='vote'),
     url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
+    url(r"^(?P<pk>\d+)/results/api_get/$", views.ajax_get, name="ajax_get"),
     url(r"^thanks/$", views.ThanksPage.as_view(), name="thanks"),
     url(r'^login/$', LoginView.as_view(template_name="admin/login.html"), name='login'),
     url(r'^logout/$', LogoutView.as_view(template_name="admin/base.html"), name='logout'),
--- vote/views.py
+++ vote/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.http import JsonResponse
 
 
 class VotePage(ListView):
@@ -62,3 +63,20 @@ def vote(request, pk):
         selected_choice.save()
         return redirect("vote:thanks")
 
+
+def ajax_get(request, pk):
+    if request.method == "GET":
+        question = get_object_or_404(Question, pk=pk)
+        choices = question.choice_set.all()
+        choice_list = []
+        for choice in choices:
+            choice_contents = {
+                "choice_text": choice.choice_text,
+                "votes": choice.votes,
+            }
+            choice_list.append(choice_contents)
+
+        choice_json = {
+            "choice_list": choice_list
+        }
+        return JsonResponse(data=choice_json)
