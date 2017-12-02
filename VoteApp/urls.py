f6a4021de29fce807abbb512ca13e73c3e28b73b
Shimbo Takaaki <ta7uwtaka@gmail.com>
2017-12-02 21:59:16 +0900

    Fix urls.py

--- VoteApp/urls.py
+++ VoteApp/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r"^vote/", include("vote.urls", namespace="vote")),
+    url(r"^", include("vote.urls", namespace="vote")),
 
 ]
