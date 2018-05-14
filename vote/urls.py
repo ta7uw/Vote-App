ef2054ce9ab3c7a96a39bc170b009c4c36f99fc5
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-05-15 07:49:00 +0900

    Add: app_name

--- vote/urls.py
+++ vote/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
-
+app_name = "vote"
 urlpatterns = [
     url(r'^$', views.VotePage.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.QuestionDetail.as_view(), name='detail'),
@@ -12,6 +12,5 @@ urlpatterns = [
     url(r"^thanks/$", views.ThanksPage.as_view(), name="thanks"),
     url(r'^login/$', LoginView.as_view(template_name="vote/login.html"), name='login'),
     url(r'^logout/$', LogoutView.as_view(template_name="admin/base.html"), name='logout'),
-
 
 ]
